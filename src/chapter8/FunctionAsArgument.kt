eca80082a061119495ed98cc9882d5045512d17a
Richard Walker <richard.walker.90@gmail.com>
2018-03-20 20:24:42 -0400

    Create function that filters char from strings

--- src/chapter8/FunctionAsArgument.kt
+++ src/chapter8/FunctionAsArgument.kt
@@ -1,9 +1,23 @@
 package chapter8
 
 fun main(args: Array<String>) {
-    println("Multiplying 2 * 3 = ${twoAndThree{ a, b -> a * b}}")
-    println("Addition 2 + 3 = ${twoAndThree{ a, b -> a + b}}")
+    println("Multiplying 2 * 3 = ${twoAndThree { a, b -> a * b }}")
+    println("Addition 2 + 3 = ${twoAndThree { a, b -> a + b }}")
+    println("Converting to Hashcode: ${convertTestStringIntoNumber { String.hashCode() }}")
+    println("abc1234XYZrichie".filterOutChars { it in 'a'..'z' })
 }
 
 fun twoAndThree(operation: (Int, Int) -> Int) = operation(2, 3)
+
+fun convertTestStringIntoNumber(lambda: (String) -> Int) = lambda("TestString")
+
+fun String.filterOutChars(predicate: (Char) -> Boolean): String {
+    val builder = StringBuilder()
+    (0 until length).forEach { index ->
+        val charInString = get(index)
+        if (predicate(charInString)) builder.append(charInString)
+    }
+    return builder.toString()
+}
+
 
